c80166962839d9090c5ac7cdb5c2d5b9c2b34676
nexus <38168516+nexus4880@users.noreply.github.com>
2024-07-10 16:41:32 -0700

    Fix issue #50

--- src/controllers/FikaDialogueController.ts
+++ src/controllers/FikaDialogueController.ts
@@ -62,6 +62,7 @@ export class FikaDialogueController {
                     Level: profile.Info.Level,
                     Side: profile.Info.Side,
                     MemberCategory: profile.Info.MemberCategory,
+                    SelectedMemberCategory: profile.Info.MemberCategory,
                 },
             });
         }
